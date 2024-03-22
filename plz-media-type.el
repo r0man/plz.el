e7aa7c689bf0953a2677223b4792157586e311e8
Roman Scherer <roman@burningswell.com>
2024-03-22 12:43:05 +0100

    Add package description

--- plz-media-type.el
+++ plz-media-type.el
@@ -24,7 +24,13 @@
 
 ;;; Commentary:
 
-;; This file handles content type.
+;; This library provides enhanced handling of MIME types for HTTP
+;; requests within Emacs.  It utilizes the 'plz' library for
+;; networking calls, extending it to process responses based on the
+;; Content-Type header.  This library defines various classes and
+;; methods for parsing and processing standard MIME types, including
+;; JSON, XML, HTML, and binary data.  It allows for extensible
+;; processing of additional types through subclassing.
 
 ;;; Code:
 
